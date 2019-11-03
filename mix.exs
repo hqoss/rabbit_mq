9abaf0240d423ca1a41fa51f7072ae0debff5a16
Slavomir Vojacek <slavomirvojacek@gmail.com>
2019-11-03 14:33:06 +0000

    chore(Version): Pre-release 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MQ.MixProject do
   def project do
     [
       app: :rabbit_mq_ex,
-      version: "0.1.1",
+      version: "1.0.0-0.0.1",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
