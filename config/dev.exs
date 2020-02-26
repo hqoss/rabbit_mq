b68742859c310898a6c7b993983369caa54ac641
Slavo Vojacek <me@slavovojacek.com>
2020-02-26 16:05:42 +0000

    chore(Deps): Upgrade deps

--- config/dev.exs
+++ config/dev.exs
@@ -1,3 +1,3 @@
-use Mix.Config
+import Config
 
 config :rabbit_mq_ex, :amqp_url, "amqp://guest:guest@localhost:5672"
--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,4 @@
-use Mix.Config
+import Config
 
 config :rabbit_mq_ex, :amqp_url, "amqp://guest:guest@localhost:5672"
 
