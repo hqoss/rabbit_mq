b773a9ae33a69dd419bf7796dab386078580e615
Slavo Vojacek <public@slavovojacek.com>
2020-05-01 17:23:39 +0100

    chore(mix): Update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MQ.MixProject do
   def project do
     [
       app: :rabbit_mq,
-      version: "0.0.0-alpha-1",
+      version: "0.0.0-alpha-2",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
