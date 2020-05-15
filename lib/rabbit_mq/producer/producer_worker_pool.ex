a228077f21a38ca3b94593c29803c7ecddd568b8
Slavo Vojacek <public@slavovojacek.com>
2020-05-15 18:47:44 +0100

    test(ProducerWorkerPool): Add tests for Producer Worker Pool

---
+++ lib/rabbit_mq/producer/producer_worker_pool.ex
@@ -0,0 +1,38 @@
+defmodule RabbitMQ.Producer.WorkerPool do
+  alias RabbitMQ.Producer.Worker
+
+  use Supervisor
+
+  @this_module __MODULE__
+
+  @worker_pool_opts ~w(connection exchange handle_publisher_ack handle_publisher_nack worker worker_count)a
+
+  def start_link(opts) do
+    name = Keyword.fetch!(opts, :name)
+    worker_pool_opts = Keyword.take(opts, @worker_pool_opts)
+
+    Supervisor.start_link(@this_module, worker_pool_opts, name: name)
+  end
+
+  @impl true
+  def init(worker_pool_opts) do
+    worker_count = Keyword.fetch!(worker_pool_opts, :worker_count)
+    worker = Keyword.fetch!(worker_pool_opts, :worker)
+
+    opts = Keyword.delete(worker_pool_opts, :worker_count)
+
+    children =
+      1..worker_count
+      |> Enum.with_index()
+      |> Enum.map(fn {_, index} ->
+        opts = Keyword.put(opts, :name, Module.concat(worker, "#{index}"))
+
+        %{
+          id: index,
+          start: {Worker, :start_link, [opts]}
+        }
+      end)
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
---
+++ test/rabbit_mq/producer/producer_worker_pool_test.exs
@@ -0,0 +1,65 @@
+defmodule RabbitMQTest.Producer.WorkerPool do
+  alias RabbitMQ.Producer.WorkerPool
+  alias RabbitMQ.Producer.Worker
+
+  use AMQP
+  use ExUnit.Case
+
+  @exchange "#{__MODULE__}"
+
+  @connection __MODULE__.Connection
+  @worker __MODULE__.Worker
+  @worker_count 3
+  @worker_pool __MODULE__
+
+  @producer_worker_opts [
+    connection: @connection,
+    exchange: @exchange,
+    name: @worker_pool,
+    worker: @worker,
+    worker_count: @worker_count
+  ]
+
+  setup_all do
+    assert {:ok, _pid} = start_supervised({RabbitMQ.Connection, [name: @connection]})
+    :ok
+  end
+
+  test "start_link/1 starts a supervised worker pool with correctly named children" do
+    assert {:ok, _pid} = start_supervised({WorkerPool, @producer_worker_opts})
+
+    assert [
+             {2, worker_2, :worker, [RabbitMQ.Producer.Worker]},
+             {1, worker_1, :worker, [RabbitMQ.Producer.Worker]},
+             {0, worker_0, :worker, [RabbitMQ.Producer.Worker]}
+           ] = Supervisor.which_children(@worker_pool)
+
+    assert worker_0 === Process.whereis(Module.concat(@worker, "0"))
+    assert worker_1 === Process.whereis(Module.concat(@worker, "1"))
+    assert worker_2 === Process.whereis(Module.concat(@worker, "2"))
+  end
+
+  test "all worker pool children use correctly configured exchange" do
+    assert {:ok, _pid} = start_supervised({WorkerPool, @producer_worker_opts})
+
+    # Pick a random child (index)
+    child_id = 0..2 |> Enum.random() |> Integer.to_string()
+
+    assert %Worker.State{
+             exchange: @exchange
+           } = @worker |> Module.concat(child_id) |> :sys.get_state()
+  end
+
+  test "all worker pool children re-use the same connection" do
+    assert {:ok, _pid} = start_supervised({WorkerPool, @producer_worker_opts})
+
+    assert %Connection{pid: connection_pid} = GenServer.call(@connection, :get)
+
+    # Pick a random child (index)
+    child_id = 0..2 |> Enum.random() |> Integer.to_string()
+
+    assert %Worker.State{
+             channel: %Channel{conn: %{pid: ^connection_pid}}
+           } = @worker |> Module.concat(child_id) |> :sys.get_state()
+  end
+end
